64eded1566e7e1ba740a5705a92ea01c602898bd
Denis Filippov <filippov.den92@gmail.com>
2016-01-13 13:50:19 +0200

    ready to work

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -3,14 +3,8 @@
     {
         public function index()
         {
-            $data['user_sex'] = '';
-                if ($this->session->userdata('user_sex'))
-                {
-                  $user_sex['user_sex'] = $this->session->userdata('user_sex');
-                }
-            $data['css'] = 'main';
 
-            $this->load->view('user_interface/header', $data);
+            $this->load->view('user_interface/header');
             $this->load->view('user_interface/main_page');
             $this->load->view('user_interface/footer');
         }
