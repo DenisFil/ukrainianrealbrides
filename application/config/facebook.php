fc267b4cfa22eda20f9307f4cdad78f4ac1bbdc7
Denis Filippov <filippov.den92@gmail.com>
2016-01-20 12:56:38 +0200

    D:/Git

---
+++ application/config/facebook.php
@@ -0,0 +1,7 @@
+<?php
+    define('URL_AUTH', 'https://www.Facebook.com/dialog/oauth');
+    define('CLIENT_ID', '427767240752729');
+    define('SECRET', '38e3ede8c5b2b8ce1682536bcff55823');
+    define('REDIRECT', 'http://ukrainianrealbrides.int/user_interface/signup');
+    define('TOKEN', 'https://graph.Facebook.com/oauth/access_token');
+    define('GET_DATA', 'https://graph.Facebook.com/me');
