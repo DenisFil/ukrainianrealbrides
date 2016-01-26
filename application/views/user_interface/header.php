53a5d92a7bdcf09c901610f82b96bae3d2847f1d
DmytroT <tyzov@bk.ru>
2016-01-26 15:22:45 +0200

    Yesterday fixes

--- application/views/user_interface/header.php
+++ application/views/user_interface/header.php
@@ -3,7 +3,7 @@
 <head>
     <title>Ukrainian Real Brides</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, maximum-scale=1, target-densityDPI=device-dpi"/>
+    <meta name="viewport" content="width=device-width, maximum-scale=1"/>
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <meta name="format-detection" content="telephone=no" />
 
