d7616c90407c3c44664e3f08d35a66a2fca7de37
Denis Filippov <filippov.den92@gmail.com>
2016-01-27 11:51:00 +0200

    color js done

--- content/user_interface/js/personal_area.js
+++ content/user_interface/js/personal_area.js
@@ -2,11 +2,21 @@ $(document).ready(function(){
 //Изменение иконок в header`e
     $('.status-bar em').each(function(){
         var text = $(this).text();
-        var cell = $(this).attr('name');
         if (text == 0){
-            $('.' + cell + '-status').removeClass('points');
+            $(this).parent().removeClass('points');
         }else{
-            $('.' + cell + '-status').addClass('points');
+            $(this).parent().addClass('points');
+        }
+    });
+
+//Изменение ссылок online
+    $('.inbox-online span').each(function(){
+        var text = $(this).text();
+        console.log(text);
+        if(text == 0){
+            $(this).parent().removeClass('points');
+        }else{
+            $(this).parent().addClass('points');
         }
     });
 });
