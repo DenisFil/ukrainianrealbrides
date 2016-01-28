4056390c106d33102274bf6f41d944b87b618b52
Denis Filippov <filippov.den92@gmail.com>
2016-01-28 14:02:38 +0200

    D:/Git

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -8,9 +8,13 @@
 
             $data['css'] = 'main';
             $data['gender'] = '';
-            $data['new_messages'] = $this->personal_area_model->get_new_messages($user_id);
-            $data['users_online'] = $this->personal_area_model->users_online(time());
-            $data['credits'] = $this->personal_area_model->user_credits($user_id);
+                if ($user_id != '')
+                {
+                    $data['new_messages'] = $this->personal_area_model->get_new_messages($user_id);
+                    $data['users_online'] = $this->personal_area_model->users_online(time());
+                    $data['credits'] = $this->personal_area_model->user_credits($user_id);
+                }
+
                 if ($this->session->userdata('gender'))
                 {
                     $data['gender'] = $this->session->userdata('gender');
