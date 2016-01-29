9a6ad119300fdb5d4f6dcd3068fef5d0dd03fb56
Denis Filippov <filippov.den92@gmail.com>
2016-01-29 17:56:33 +0200

    D:/Git

--- application/views/user_interface/personal_area.php
+++ application/views/user_interface/personal_area.php
@@ -3,15 +3,15 @@
     <div class="profile-block">
         <h2>Welcome <?php echo $this->session->userdata('name'); ?></h2>
 		<span class="profile-photo">
-<!-- 
-		    <label class="file_upload">
-                <img src="<?php echo base_url(); ?>content/user_interface/img/personal-area/upload-image.png" width="34" height="27" alt="Upload image">
-                <span>Add Profile Photo</span>
-                <form id="avatar"><input type="file" id="avatar-photo" name="avatar"></form>
-            </label>
- -->
-            <img src="<?php echo base_url(); ?>content/user_interface/img/personal-area/profile-photo.jpg" width="186" height="281" alt="Profile photo">
-
+            <?php if (!$avatar): ?>
+                <label class="file_upload">
+                    <img src="<?php echo base_url(); ?>content/user_interface/img/personal-area/upload-image.png" width="34" height="27" alt="Upload image">
+                    <span>Add Profile Photo</span>
+                    <form id="avatar"><input type="file" id="avatar-photo" name="avatar"></form>
+                </label>
+            <?php else: ?>
+                <img src="<?php echo base_url(); ?>content/profiles/avatars/<?php echo $avatar; ?>_avatar.jpg" width="186" height="281" alt="Profile photo">
+            <?php endif; ?>
 		</span>
         <div class="profile-main">
             <ul>
