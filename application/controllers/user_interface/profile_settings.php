af7d5cccdc0dbbd30f0ce690d09d308428d5d876
Denis Filippov <filippov.den92@gmail.com>
2016-02-03 15:57:05 +0200

    profileSettings start

---
+++ application/controllers/user_interface/profile_settings.php
@@ -0,0 +1,25 @@
+<?php
+    class Profile_settings extends CI_Controller
+    {
+        public function index()
+        {
+            if ($this->session->userdata('id'))
+            {
+                $user_id = $this->session->userdata('id');
+
+                $data['new_messages'] = $this->personal_area_model->get_new_messages($user_id);
+                $data['users_online'] = $this->personal_area_model->users_online(time());
+                $data['credits'] = $this->personal_area_model->user_credits($user_id);
+                $data['css'] = 'profile_settings';
+                $data['gender'] = '';
+                if ($this->session->userdata('gender'))
+                {
+                    $data['gender'] = $this->session->userdata('gender');
+                }
+
+                $this->load->view('user_interface/header', $data);
+                $this->load->view('user_interface/profile_settings');
+                $this->load->view('user_interface/footer');
+            }
+        }
+    }
