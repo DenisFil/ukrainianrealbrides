eee98cc279396be8648b32119426f7c99de89d1a
Denis Filippov <filippov.den92@gmail.com>
2016-02-16 16:13:36 +0200

    D:/Git

--- application/views/user_interface/personal_area.php
+++ application/views/user_interface/personal_area.php
@@ -19,7 +19,7 @@
                     </form>
                 </label>
 
-                <a class="preview" href="<?php echo base_url(); ?>/user_interface/profile_preview">Preview</a>
+                <a class="preview" href="<?php echo base_url(); ?>/user_interface/my_profile_preview">Preview</a>
             </div>
             <?php endif; ?>
 		</span>
