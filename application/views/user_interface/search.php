bb1e9403190ca9d1cf93b9331af58a79bb00a551
DmytroT <tyzov@bk.ru>
2016-02-18 19:05:05 +0200

    search

--- application/views/user_interface/search.php
+++ application/views/user_interface/search.php
@@ -2,7 +2,7 @@
 	<div class="search">
 		<h2>Search</h2>
 		<span class="sub-heading">Sub heading</span>
-		<div class="search-content">
+		<form class="search-content" action="#">
 			<div class="search-id">
 				<span class="search-title">ID</span>
 				<div class="search-id-field">
@@ -16,8 +16,34 @@
 			</div>
 			<div class="search-age">
 				<span class="search-title">Age</span>
-
+	            <div class="search-range">
+	                <em class="left-range">21</em>
+	               	<div class="slider-wrap">
+	                	<div id="slider"></div>
+	                </div>
+	                <em class="right-range">50</em>
+				</div>	
+			</div>
+			<div class="search-country">
+				<span class="search-title">Country</span>
+				<select >
+					<option selected value="">Ukraine</option>
+					<option value="">Ukraine</option>
+					<option value="">Ukraine</option>
+					<option value="">Ukraine</option>
+				</select>
+			</div>
+		</form>
+	</div>
+	<div class="search-results">
+		<div class="search-profile-block">
+			<img src="<?php echo base_url(); ?>content/user_interface/img/profile-preview/preview-avatar.jpg" alt="Profile photo" width="196" height="298" >
+			<div class="search-profile-info">
+				<strong>Marina</strong>
+				<span>25, Ukraine</span>
+				<em>Online</em>
 			</div>
 		</div>
+
 	</div>
 </div>
