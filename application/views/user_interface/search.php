ca64fe720929c153c56c6abf29a973827f3f38a9
DmytroT <tyzov@bk.ru>
2016-02-22 16:06:28 +0200

    search

--- application/views/user_interface/search.php
+++ application/views/user_interface/search.php
@@ -5,23 +5,19 @@
 		<form class="search-content" action="#">
 			<div class="search-id">
 				<span class="search-title">ID</span>
-				<div class="search-id-field">
-	                <input class="id-form" type="text" placeholder="023" id="#" >
-	                <span>
-	                	<a href="#">
-	                		<img src="<?php echo base_url(); ?>content/user_interface/img/search/search-id.png" width="22" height="22" alt="Question icon">
-	                	</a>
-	                </span>
+				<div class="search-id-holder">
+		            <input class="search-form" type="text" placeholder="023" id="#" >
+					<button type="button" class="search-id-button"></button>
 				</div>
 			</div>
 			<div class="search-age">
 				<span class="search-title">Age</span>
 	            <div class="search-range">
-	                <em class="left-range">21</em>
+	                <em class="left-range">18</em>
 	               	<div class="slider-wrap">
 	                	<div id="slider"></div>
 	                </div>
-	                <em class="right-range">50</em>
+	                <em class="right-range">90</em>
 				</div>	
 			</div>
 			<div class="search-country">
@@ -40,23 +36,21 @@
 				<span class="search-title">Status</span>
 					<label class="switch-light switch-candy" onclick="">
 					  <input type="checkbox">
-					  <span>
-					    <span>All</span>
-					    <span>Online</span>
+					  <span class="switch-holder">
+					    <span class="left-switch">All</span>
+					    <span class="right-switch">Online</span>
 					    <a></a>
 					  </span>
 					</label>			
-
-
-
 			</div>
+			<button type="button" class="view-profile-button search-top">Search</button>
 		</form>
 	</div>
 	<div class="search-results">
 		<div class="search-profile-block">
 			<img src="<?php echo base_url(); ?>content/user_interface/img/profile-preview/preview-avatar.jpg" alt="Profile photo" width="196" height="298" >
 			<div class="search-profile-info">
-				<strong>Anastasia, <span>25</span></strong>
+				<strong>Anastasia, 25</strong>
 				<span>Ukraine, Kharkiv</span>
 				<em>Online</em>
 				<ul>
@@ -85,7 +79,7 @@
 		<div class="search-profile-block">
 			<img src="<?php echo base_url(); ?>content/user_interface/img/profile-preview/preview-avatar.jpg" alt="Profile photo" width="196" height="298" >
 			<div class="search-profile-info">
-				<strong>Anastasia, <span>25</span></strong>
+				<strong>Anastasia, 25</strong>
 				<span>Ukraine, Kharkiv</span>
 				<em>Online</em>
 				<ul>
@@ -114,7 +108,7 @@
 		<div class="search-profile-block">
 			<img src="<?php echo base_url(); ?>content/user_interface/img/profile-preview/preview-avatar.jpg" alt="Profile photo" width="196" height="298" >
 			<div class="search-profile-info">
-				<strong>Anastasia, <span>25</span></strong>
+				<strong>Anastasia, 25</strong>
 				<span>Ukraine, Kharkiv</span>
 				<em>Online</em>
 				<ul>
@@ -143,7 +137,7 @@
 		<div class="search-profile-block">
 			<img src="<?php echo base_url(); ?>content/user_interface/img/profile-preview/preview-avatar.jpg" alt="Profile photo" width="196" height="298" >
 			<div class="search-profile-info">
-				<strong>Anastasia, <span>25</span></strong>
+				<strong>Anastasia, 25</strong>
 				<span>Ukraine, Kharkiv</span>
 				<em>Online</em>
 				<ul>
