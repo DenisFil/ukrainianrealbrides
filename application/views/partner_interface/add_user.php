d24962edbef11b5be9bb939e5f3a4a284293ce78
DmytroT <tyzov@bk.ru>
2016-03-22 10:20:41 +0200

    panel

---
+++ application/views/partner_interface/add_user.php
@@ -0,0 +1,341 @@
+<div class="container add-user-block">
+	<div class="col-md-6 left-side-reset">
+		<div class="col-md-4 left-side-reset avatar-fixes">
+			<h4 class="text-center">Аватар</h4>
+<!-- 
+			<a href="#" class="thumbnail avatar-thumb">
+				<img src="http://ukrainianrealbrides.int/content/profiles/avatars/22/126e3e21f3022ec2dd7af8a72d393e24_avatar.jpg" alt="Аватар профиля" width="186" height="281">
+			</a>
+-->
+			<a class="thumbnail avatar-upload">
+				<div class="fileUpload">
+					<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+					<input type="file" class="upload" />
+				</div>	
+			</a>
+		</div>
+		<div class="col-md-8">
+			<h4 class="text-center">Фотографии профиля</h4>
+			<div class="row thumbnail profile-photos">
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<a href="#" class="small-photo">
+					<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Фото профиля">
+				</a>
+				<div class="fileUpload">
+					<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+					<input type="file" class="upload" />
+				</div>
+			</div>
+			<div class="row video-passport">
+				<div class="col-md-6 video-block left-col">
+					<h4 class="text-center">Видео</h4>
+					<a href="#">
+						<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="Видео профиля">
+					</a>
+					<div class="fileUpload">
+						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+						<input type="file" class="upload" />
+					</div>
+				</div>
+				<div class="col-md-6 pass-block right-col">
+						<h4 class="text-center">
+						Скан паспорта<span class="tool-tip" aria-hidden="true" data-tooltip="Загрузите скан или фотографии вашего паспорта (страницы с фото и прописка)"><span class="glyphicon glyphicon-question-sign"></span></span>
+						</h4>
+					<div class="col-md-12 text-center">
+						<a href="#">
+							<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="">
+						</a>
+						<a href="#">
+							<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="">
+						</a>
+						<a href="#">
+							<img class="image-responsive" src="http://ukrainianrealbrides.int/content/profiles/photo/22/d5eaa9b41495274bed469ae50af733b1_preview.jpg" alt="">
+						</a>
+						<div class="fileUpload" >
+							<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+							<input type="file" class="upload" />
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="col-md-12 general-info general-headings text-center">
+			<h4>Общая информация</h4>
+			<h6>Необходимо вносить только достоверную информацию на английском языке</h6>
+			<div class="form-group col-md-6 left-col ">
+			   <input type="text" class="form-control" id="#" placeholder="Имя">
+			   <span class="error-message">Error message</span>
+			   <input type="text" class="form-control" id="#" placeholder="Фамилия">
+			   <span class="error-message">Error message</span>
+			   <input type="text" class="form-control" id="#" placeholder="E-mail">
+			   <span class="error-message">Error message</span>
+			   <input type="text" class="form-control" id="#" placeholder="Пароль для входа">
+			   <span class="error-message">Error message</span>
+			</div>
+			<div class="col-md-6 right-col">
+				<div class="col-md-12 birth-date">
+					<div class="form-group col-md-4 row-push">
+						<select class="form-control" id="#">
+						  <option>День </option>
+						  <option>01</option>
+						  <option>02</option>
+						  <option>03</option>
+						</select>	
+					</div>
+					<div class="form-group col-md-4 row-push">
+						<select class="form-control" id="#">
+						  <option>Месяц</option>
+						  <option>01</option>
+						  <option>02</option>
+						  <option>03</option>
+						</select>	
+					</div>
+					<div class="form-group col-md-4">
+						<select class="form-control" id="#">
+						  <option>Год</option>
+						  <option>1990</option>
+						  <option>1991</option>
+						  <option>1992</option>
+						  <option>1992</option>
+						</select>	
+					</div>
+			   		<span class="error-message">Error message</span>
+				</div>
+
+
+			   <input type="text" class="form-control" id="#" placeholder="Страна">
+			   <span class="error-message">Error message</span>
+			   <input type="text" class="form-control" id="#" placeholder="Город">
+			   <span class="error-message">Error message</span>
+			   <input type="text" class="form-control" id="#" placeholder="Контактный телефон">
+			   <span class="error-message">Error message</span>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3 text-center general-info selects-colls about-me">
+		<h4>О себе</h4>
+		<div class="col-md-6 left-col">
+			<select id="" class="form-control">
+				<option value="">Рост</option>
+			</select>
+			<select id="" class="form-control">
+				<option value="">Цвет глаз</option>
+			</select>
+			<select id="" class="form-control">
+				<option value="">Дети</option>
+			</select>
+			<select id="" class="form-control">
+				<option value="">Курение</option>
+			</select>
+			<select id="" class="form-control">
+				<option value="">Образование</option>
+			</select>
+		</div>
+		<div class="col-md-6 right-col">
+			<select id="" class="form-control">
+				<option value="">Вес</option>
+			</select>
+			<select id="" class="form-control">
+				<option value="">Цвет волос</option>
+			</select>
+			<select id="" class="form-control">
+				<option value="">Религия</option>
+			</select>
+			<select id="" class="form-control">
+				<option value="">Алкоголь</option>
+			</select>
+			<div class="col-md-12 tool-tip romantic-tours" data-tooltip="Согласна ли девушка отправиться в романтическое путешествие.">
+				<select id="" class="form-control" >
+					<option value="">Ром. туры</option>
+					<option value="">Да</option>
+					<option value="">Нет</option>
+				</select>
+			</div>
+
+		</div>
+		<div class="col-md-12">
+			<textarea id="" class="form-control about-me-form" cols="30" rows="5" placeholder="Обо мне / About me"></textarea>
+			<span class="error-message">Error message</span>
+		</div>
+		<div class="col-md-12">
+			<textarea id="" class="form-control hobby-form" cols="30" rows="5" placeholder="Хобби"></textarea>
+			<span class="error-message">Error message</span>
+		</div>
+	</div>
+	<div class="col-md-3 text-center general-info selects-colls about-partner">
+		<h4>О партнере</h4>
+		<div class="col-md-6 left-col">
+			<div class="col-md-6 left-col">
+				<select id="" class="form-control">
+					<option value="">От</option>
+				</select>
+			</div>
+			<div class="col-md-6  right-col">
+				<select id="" class="form-control">
+					<option value="">До</option>
+				</select>
+			</div>
+			<select id="" class="form-control">
+				<option value="">Алкоголь</option>
+			</select>
+		</div>
+		<div class="col-md-6 right-col">
+			<select id="" class="form-control">
+				<option value="">Дети</option>
+			</select>
+			<select id="" class="form-control">
+				<option value="">Курение</option>
+			</select>
+		</div>
+		<div class="col-md-12">
+			<textarea id="" class="form-control area-sm" cols="30" rows="3" placeholder="О партнере / About partner"></textarea>
+			<span class="error-message">Error message</span>
+		</div>
+		<div class="col-md-12 tool-tip" aria-hidden="true" data-tooltip="Выберите оператора за которым будет закреплена анкета. Вы сможете изменить его на вкладке Анкеты">
+			<select id="" class="form-control bottom-push" >
+				<option value="">Оператор</option>
+			</select>
+		</div>
+		<div class="col-md-12 text-left">
+			<h4 class="text-center notifications-heading">Email оповещения</h4>
+			<div class="checkbox">
+			  <label><input type="checkbox" value="" checked>Новости и обновления</label>
+			</div>
+			<div class="checkbox">
+			  <label><input type="checkbox" value="" checked>Сообщения и приглашения</label>
+			</div>
+			<div class="checkbox">
+			  <label><input type="checkbox" value="" checked>Акции</label>
+			</div>
+			<div class="text-center">
+				Подтверждаю что девушка ознакомлена с <a href="#">Условиями использования</a> и <a href="#">Политикой конфинденциальности</a>	
+			</div>
+
+			<button class="btn btn-primary button-submit">Сохранить в "Не активные"</button>
+			<button class="btn btn-success button-submit">Отправить на подтверждение</button>
+		</div>
+	</div>
+</div>
+
+
+<!-- 
+
+<div class="container-fluid">
+	<div class="col-md-6">
+		<div class="row">
+			<div class="col-md-3 text-center" >
+				<h4>Аватар</h4>
+				<span class="thumbnail">
+					<img src="http://adamkuszczak.azurewebsites.net/Files/AdamKuszczak_photo_empty.jpg" alt="">
+				</span>
+			</div>
+			<div class="col-md-7 text-center">
+				<div class="row">
+					<h4>Фото профиля</h4>
+					<ul class="col-md-12 profile-photos thumbnail">
+						<li class="col-md-2">
+							<a href="#" class="">
+								<img class="image-responsive" src="http://www.manrepeller.com/wp-content/uploads/2014/03/selfiefbPP.jpg" alt="">
+							</a>
+						</li>
+						<li class="col-md-2">
+							<a href="#" class="thumbnail new-photo">
+								<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="row video-pas">
+					<div class="col-md-6 video-block">
+						<h4>Видео</h4>
+						<div class="col-md-12 thumbnail video-block-height">
+							<div class="col-md-6">
+								<a href="#" class="thumbnail">
+									<img class="image-responsive" src="http://www.samsung.com/global/galaxy/galaxy-s6-edge-plus/gallery/1/thumb_gallery.jpg" alt="">
+								</a>	
+							</div>
+							<div class="col-md-6">
+								<a href="#" class="thumbnail">
+									<img class="image-responsive" src="http://www.samsung.com/global/galaxy/galaxy-s6-edge-plus/gallery/1/thumb_gallery.jpg" alt="">
+								</a>	
+							</div>
+							<div class="col-md-6">
+								<a href="#" class="thumbnail">
+									<img class="image-responsive" src="http://www.samsung.com/global/galaxy/galaxy-s6-edge-plus/gallery/1/thumb_gallery.jpg" alt="">
+								</a>	
+							</div>
+							<div class="col-md-6">
+								<a href="#" class="thumbnail add-video">
+									<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+								</a>
+							</div>
+						</div>
+
+					</div>
+					<div class="col-md-6">
+						<h4>Паспорт</h4>
+						<div class="col-md-12 pass-block thumbnail">
+							<a href="#">
+								<img class="image-responsive" src="http://img.thesun.co.uk/aidemitlum/archive/02513/01_a254afae-6b62-1_2513098a.jpg" alt="">
+							</a>	
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<h3 class="text-center">Общая информация</h3>
+			<div class="row">
+				<div class="col-md-6">
+					<input type="email" class="form-control" id="#">
+				</div>
+				<div class="col-md-6">
+					<input type="email" class="form-control" id="#">
+				</div>
+			</div>
+		</div>
+	</div>
+</div> 
+-->
--- application/views/partner_interface/footer.php
+++ application/views/partner_interface/footer.php
@@ -2,9 +2,7 @@
     <p>UkrainianRealBrides 2001-2016 &copy; All rights reserved.</p>
   </footer>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	<script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
 	<script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
